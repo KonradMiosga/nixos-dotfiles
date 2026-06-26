0745d4b8caba01a5c10ba6b5a1142e5420df60c6
Konrad Miosga <konrad.miosga@gmail.com>
2026-06-26 10:31:08 +0200

    better man page cache

--- configuration.nix
+++ configuration.nix
@@ -13,8 +13,7 @@
 
   time.timeZone = "Europe/Berlin";
 
-  documentation.man.generateCaches = true;
-
+  documentation.man.cache.enable = true;
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
